1356a38f5feebe4ebf8c6edf9701776a2f5e1a7d
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2021-07-04 20:03:27 +0530

    Add files via upload

---
+++ Array/palindrome_character_array.cpp
@@ -0,0 +1,21 @@
+//Find if a word is palindrome or not
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    char arr[n+1];
+    cin>>arr;
+    bool check=1;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=arr[n-1-i]){
+            check=0;
+            break;
+        }
+    }
+    if(check==true)
+    cout<<"Word is palindrome"<<endl;
+    else
+    cout<<"Word is not palindrome"<<endl;
+    return 0;
+}
