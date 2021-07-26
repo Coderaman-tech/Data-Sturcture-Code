14e9c98df6d10374370eb8c9177d604cafcca579
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2021-07-26 16:34:26 +0530

    Create string.basics.cpp

---
+++ Strings/string.basics.cpp
@@ -0,0 +1,18 @@
+//See all basics of string
+#include<iostream>
+#include<string>  //for string functions
+using namespace std;
+int main()
+{
+    //Declare a string
+   /* string str;
+    cin>>str;
+    cout<<str<<endl;*/
+    //for string with space use
+    string s;
+    getline(cin,s);
+    cout<<s<<endl;
+
+
+    return 0;
+}
