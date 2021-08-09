1c14b0985e26a10016092c71675aef355db05ee9
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2021-08-09 23:46:49 +0530

    Create Pair.cpp

---
+++ Pair.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //pair<data_type1,data_type2>variable_name;
+    
+    pair<int ,char>p;
+    p.first=123;
+    p.second='d';
+    cout<<p.first<<" "<<p.second<<endl; //123 d
+    
+}
