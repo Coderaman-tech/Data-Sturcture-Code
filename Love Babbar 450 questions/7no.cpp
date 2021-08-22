bfcc2a93f64abc06d4f6ba97c5ce5c661b4a5f93
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2021-08-22 12:19:46 +0530

    Create 7no.cpp

---
+++ Love Babbar 450 questions/7no.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    int maximum=arr[0],minimum=arr[0];
+  for(int i=0;i<n;i++){
+      maximum=max(maximum,arr[i]);
+      minimum=min(minimum,arr[i]);
+  }
+  cout<<maximum<<" "<<minimum;
+}
