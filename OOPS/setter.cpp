f88f3f7272bdcd3aebbc09a50ad154b3d60183b9
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2021-09-21 20:21:47 +0530

    Create setter.cpp

---
+++ OOPS/setter.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class student{
+  string name;
+  public:
+  int age;
+  bool gender;
+  void setName(string s){//Use setter concept
+      name=s;
+  }
+  void printInfo(){
+    cout<<"Name: ";
+    cout<<name<<endl;
+    cout<<"Age: ";
+    cout<<age<<endl;
+    cout<<"Gender: ";
+    cout<<gender<<endl;
+  }
+};
+int main(){
+  student arr[3];
+  for(int i=0;i<3;i++){
+    string s;
+    cout<<"Name= ";
+    cin>>s;
+    arr[i].setName(s);
+    cout<<"Age= ";
+    cin>>arr[i].age;
+    cout<<"Gender= ";
+    cin>>arr[i].gender;
+  }
+  for(int i=0;i<3;i++)
+  arr[i].printInfo();
+  return 0;
+}
