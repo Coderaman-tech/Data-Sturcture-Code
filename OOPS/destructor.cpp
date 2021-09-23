ffd9bed30409404f09decdd5df3a43a461b2e784
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2021-09-23 15:13:55 +0530

    Create destructor.cpp

---
+++ OOPS/destructor.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class student{
+  string name;
+  public:
+  int age;
+  bool gender;
+  student(){
+    cout<<"Default constructor"<<endl;
+  }
+  student(string s,int a,int g){
+    cout<<"Parametrised constructor"<<endl;
+    name=s;
+    age=a;
+    gender=g;
+  }
+  student(student &a){
+   cout<<"Copy constructor"<<endl;
+   name=a.name;
+   age=a.age;
+   gender=a.gender;
+  }
+  ~student(){
+    cout<<"Destructor called"<<endl;
+  }
+};
+int main(){
+  student a("Aman",19,0);
+  student b;
+  student c=a;
+  return 0;
+}
+/*
+Output-
+Parametrised constructor
+Default constructor
+Copy constructor
+Destructor called
+Destructor called
+Destructor called
+*/
