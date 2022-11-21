c448f2f2c588208f52da3ed50be680b6a35ff012
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2022-11-21 23:19:36 +0530

    Create letter_case_permutation.cpp

---
+++ Recursion/letter_case_permutation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    void printall(vector<string>&ans,string s,string str,int n,int ind){
+        if(ind==n){
+            ans.push_back(str);
+            return;
+        }
+        //cout<<str<<"--"<<s<<endl;
+        if(str[ind]>='a' && str[ind]<='z'){
+            printall(ans,s,str,n,ind+1);
+            str[ind]-=32;
+            printall(ans,s,str,n,ind+1);
+        }
+        else if(str[ind]>='A' && str[ind]<='Z'){
+             printall(ans,s,str,n,ind+1);
+            str[ind]+=32;
+            printall(ans,s,str,n,ind+1);
+        }
+        else{
+            printall(ans,s,str,n,ind+1);
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+        vector<string>ans;
+        string str=s;
+        int n=s.size();
+        printall(ans,s,str,n,0);
+        return ans;
+    }
+};
