0722a0d35652c7da520053dd5d9a9d4a2a5f4d36
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2023-01-24 23:31:39 +0530

    Create matrix_to_list.cpp

---
+++ Graph/matrix_to_list.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int k=max(n,m);
+        vector<int>adj[k+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && i!=j){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+    }
+};
